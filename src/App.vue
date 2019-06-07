1bcc167e5a9bf709e41cbdcc73aad600a3cbbf5a
benvae <b.vaethjunker@gmail.com>
2019-06-07 14:54:30 +0200

    - added the navbar as component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Navbar/>
   </div>
 </template>
+
+<script>
+    import Navbar from "./components/Navbar";
+    export default {
+        components: {Navbar}
+    }
+
+</script>
+
 
 <style lang="scss">
 #app {
