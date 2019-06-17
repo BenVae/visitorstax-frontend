cadab00238a92861a24d5c8d5db6773848b12275
Patrick Kratzer <patrick.kratzer@htwg-konstanz.de>
2019-06-17 18:07:47 +0200

    - added vue-material-icons dependency and added it to main.js - added vue-good-table dependency and added it to main.js - set z-index from navbar component to 100 so its always on top of the background image

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -40,7 +40,8 @@
 
 <style lang="scss" scoped>
     .navbar {
-      background-color: #FFFFFF;
-        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+        z-index: 100;
+        background-color: #FFFFFF;
+        box-shadow: 0px 8px 5px 0px rgba(0,0,0,0.25);
     }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,17 +3,21 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import BootstrapVue from 'bootstrap-vue'
+import 'vue-material-design-icons/styles.css'
+import VueGoodTablePlugin from 'vue-good-table'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'vue-good-table/dist/vue-good-table.css'
 
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
+Vue.use(VueGoodTablePlugin);
 
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
