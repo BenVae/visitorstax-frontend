9afed8f3827a64ee058601d6f8e8389c2eed38d8
mediocricity <philpierenner@web.de>
2019-06-28 23:30:49 +0200

    updated display singular form

--- src/components/DisplaySingularRegForm.vue
+++ src/components/DisplaySingularRegForm.vue
@@ -4,13 +4,15 @@
         <b-container>
             <Title :name="title"></Title>
             <div class="complete-form mt-5">
-                <b-row class="justify-content-center text-center top-buffer">
+                <b-row class="justify-content-center text-center top-buffer mb-3">
                     <b-col cols="4" id="ankunft">
-                        {{moment(form.arrivalDate).locale('de').calendar()}} bis {{moment(form.departureDate).locale('de').calendar()}}
+                        {{moment(form.arrivalDate).locale('de').calendar()}} bis
+                        {{moment(form.departureDate).locale('de').calendar()}}
                     </b-col>
                     <b-col cols="1"/>
                     <b-col cols="4">Objekt: {{form.businessObject.address.streetAndNumber}}</b-col>
                 </b-row>
+                <RowWithDescription name="Gast"/>
                 <b-row class="justify-content-center text-center top-buffer">
                     <b-col cols="4">
                         Vorname: {{form.guest.name}}
@@ -20,6 +22,8 @@
                         Familienname: {{form.guest.surname}}
                     </b-col>
                 </b-row>
+                <RowWithDescription name="Adresse"/>
+                <template v-if=""
             </div>
         </b-container>
     </Layout>
@@ -30,20 +34,22 @@
     import FormData from '../assets/sampleRegistrationForm';
     import Layout from "./utils/StandardLayout";
     import Title from "./utils/Title";
+    import RowWithDescription from "./utils/RowWithDescription";
 
 
     export default {
         name: "singularRegForm",
-        components: {Title, Layout},
+        components: {RowWithDescription, Title, Layout},
         data() {
             return {
                 form: null,
-                title: null
+                title: null,
+                hasChildren: null
             }
         },
         mounted() {
             this.form = FormData,
-                this.title = "Meldeschein #" + this.form.registrationNumber
+                this.title = "Meldeschein #" + this.form.registrationNumber;
 
         },
     }
@@ -52,11 +58,11 @@
 <style lang="scss" scoped>
 
     .complete-form {
-
+        font-size: 1.1rem;
     }
 
-    .top-buffer{
-        margin-top:10px;
+    .top-buffer {
+        margin-top: 10px;
     }
 
 </style>
