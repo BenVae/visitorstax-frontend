2b8ef31ee1ceb2259b1c04289e74903dff4691ce
benvae <b.vaethjunker@gmail.com>
2019-06-28 17:10:56 +0200

    - added table that loads data from json and displays it

--- src/components/RegistrationFormList.vue
+++ src/components/RegistrationFormList.vue
@@ -1,8 +1,31 @@
 <template>
     <standard-layout>
-        <b-container>
+        <b-container fluid>
             <Title name="Meldescheine"/>
-            <b-table striped hover bordered :fields="fields" :items="form"/>
+            <b-row class="mt-3">
+                <b-col md="4" class="my-1">
+                    <b-form-group label="Filter:" class="mb-0">
+                        <b-input-group>
+                            <b-form-input v-model="filter" placeholder="Type to Search"></b-form-input>
+                        </b-input-group>
+                    </b-form-group>
+                </b-col>
+                <b-col md="4" class="my-1">
+                    <b-form-group label="Ankunft:" class="mb-0">
+                        <b-input-group>
+                            <input type="date" v-model="ankunftFilter"/>
+                        </b-input-group>
+                    </b-form-group>
+                </b-col>
+                <b-col md="4" class="my-1">
+                    <b-form-group label="Abreise:" class="mb-0">
+                        <b-input-group>
+                            <input type="date" v-model="abfahrtFilter"/>
+                        </b-input-group>
+                    </b-form-group>
+                </b-col>
+            </b-row>
+            <b-table striped hover bordered :filter="filter" :fields="fields" :items="form"/>
         </b-container>
     </standard-layout>
 </template>
@@ -19,7 +42,10 @@
             return {
                 form: null,
                 fields:[{key:'registrationNumber',label:'Ms-Nr.', sortable:true},{key:'guest.surname', label:'Nachname'},{key:'guest.name',label:'Vorname'},
-                    {key:'arrivalDate',label:'Ankunft'},{key:'departureDate',label:'Abreise'},{key:'registrationFormTyp',label:'Typ'}]
+                    {key:'arrivalDate',label:'Ankunft'},{key:'departureDate',label:'Abreise'},{key:'registrationFormTyp',label:'Typ'}],
+                filter:null,
+                abfahrtFilter:null,
+                ankunftFilter:null
             }
         },
         mounted() {
