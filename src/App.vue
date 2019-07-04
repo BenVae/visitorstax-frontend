14503c4208488b83ea3f9367725d462b1dfa4664
Philippe Renner <philippe.renner@htwg-konstanz.de>
2019-07-04 23:56:28 +0200

    added vuetify?

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,38 @@
 <template>
-    <div id="app">
-        <Navbar/>
-        <router-view/>
-    </div>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-    import Navbar from "./components/utils/Navbar";
+import HelloWorld from './components/HelloWorld'
 
-    export default {
-        components: {Navbar}
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data () {
+    return {
+      //
     }
-
+  }
+}
 </script>
-
-<style lang="scss">
-    @import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
-    #app {
-        font-family: 'Roboto', sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        position: fixed;
-        background: url("assets/Constance.png");
-        background-size: cover;
-        height: 100vh;
-        width: 100vw;
-    }
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -10,11 +11,14 @@ import moment from 'moment';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'vue-good-table/dist/vue-good-table.css'
+import 'vuetify/dist/vuetify.min.css'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import Vuetify from 'vuetify'
 
 Object.defineProperty(Vue.prototype, 'moment', { value: moment });
 
+Vue.use(Vuetify);
 Vue.use(VueAxios, axios);
 Vue.use(BootstrapVue);
 Vue.use(VueGoodTablePlugin);
