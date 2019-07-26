2b545aca197c30b8bc6014ccdc017e8c61e89182
Philippe Renner <philippe.renner@htwg-konstanz.de>
2019-07-26 16:42:26 +0200

    started component

--- src/components/cityDashboard.vue
+++ src/components/cityDashboard.vue
@@ -1,10 +1,26 @@
 <template>
-    
+    <Layout>
+        <Title name="Dash"></Title>
+        <v-container grid-list-lg>
+            <v-layout>
+                <v-flex xs12 md6>
+                    <v-card>1</v-card>
+                </v-flex>
+                <v-flex xs12 md6>
+                    <v-card>2</v-card>
+                </v-flex>
+            </v-layout>
+        </v-container>
+    </Layout>
 </template>
 
 <script>
+    import Layout from "./utils/StandardLayout";
+    import Title from "./utils/Title"
+
     export default {
-        name: "cityDashboard"
+        name: "cityDashboard",
+        components: {Layout, Title}
     }
 </script>
 
